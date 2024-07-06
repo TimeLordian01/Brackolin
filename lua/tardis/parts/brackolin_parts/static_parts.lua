75fa5d5dced7e64354a1034244bb2ec7e4c7fdaf
TimeLordian01 <31937769+TimeLordian01@users.noreply.github.com>
2024-07-06 22:27:05 +0100

    Floor is in

--- lua/tardis/parts/brackolin_parts/static_parts.lua
+++ lua/tardis/parts/brackolin_parts/static_parts.lua
@@ -31,3 +31,55 @@ PART.Collision = true
 TARDIS:AddPart(PART)
 
 print("RoundelSet1Loaded")
+
+print("RoundelSet2Loading")
+
+local PART={}
+PART.ID = "roundelset2"
+PART.Name = "roundelset2"
+PART.Model = "models/ValeyardStudios/Brackint/roundelset2.mdl"
+PART.AutoSetup = true
+PART.Collision = true
+
+TARDIS:AddPart(PART)
+
+print("RoundelSet2Loaded")
+
+print("PillarsLoading")
+
+local PART={}
+PART.ID = "pillars"
+PART.Name = "pillars"
+PART.Model = "models/ValeyardStudios/Brackint/pillars.mdl"
+PART.AutoSetup = true
+PART.Collision = true
+
+TARDIS:AddPart(PART)
+
+print("PillarsLoaded")
+
+print("ManualFlightLoading")
+
+local PART={}
+PART.ID = "manualflight"
+PART.Name = "manualflight"
+PART.Model = "models/ValeyardStudios/Brackint/controls/ManualFlight.mdl"
+PART.AutoSetup = true
+PART.Collision = true
+
+TARDIS:AddPart(PART)
+
+print("ManualFlightLoaded")
+
+print("FloorLoading")
+
+local PART={}
+PART.ID = "floor"
+PART.Name = "floor"
+PART.Model = "models/ValeyardStudios/Brackint/Floor.mdl"
+PART.AutoSetup = true
+PART.Collision = true
+
+TARDIS:AddPart(PART)
+
+print("FloorLoaded")
